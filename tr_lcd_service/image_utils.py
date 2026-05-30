04e89f585572f00255ae8546f40b94e34b9cee10
jonty <jontyatkinson2002@gmail.com>
2026-05-30 14:23:05 +1000

    replace pixel loop in encode_rgb565 with numpy vectorisation

--- tr_lcd_service/image_utils.py
+++ tr_lcd_service/image_utils.py
@@ -1,4 +1,4 @@
-import struct
+import numpy as np
 from PIL import Image
 
 
@@ -9,17 +9,10 @@ def resize_image(img: Image.Image, width: int, height: int) -> Image.Image:
 
 def encode_rgb565(img: Image.Image) -> bytes:
     """Encode an RGB PIL image to packed big-endian RGB565 bytes."""
-    w, h = img.size
-    pixels = img.load()
-    buf = bytearray(w * h * 2)
-    idx = 0
-    for y in range(h):
-        for x in range(w):
-            r, g, b = pixels[x, y]
-            val = ((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3)
-            struct.pack_into('>H', buf, idx, val)
-            idx += 2
-    return bytes(buf)
+    arr = np.array(img, dtype=np.uint16)
+    r, g, b = arr[:, :, 0], arr[:, :, 1], arr[:, :, 2]
+    rgb565 = ((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3)
+    return rgb565.astype('>u2').tobytes()
 
 
 def pad_to_512(data: bytes) -> bytes:
